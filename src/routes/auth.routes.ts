3fba67d6c52a7b0b16a30c1d295cabba7d5c3ff5
dhwldwld <brown8167@gmail.com>
2022-04-20 00:33:18 +0900

    .

--- src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { signIn, signUp } from '../controllers/user.controller';
-const router = Router();
-
-router.post('/signup', signUp)
-router.post('/signin', signIn)
-
-export default router;
